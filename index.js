4ced7aca48da1cd482bd47c8c069b0ab369a0205
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-10 13:22:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKtten(name){
- return kittens.unshift
-  
+  kittens.unshift(name)
+  return kittens
 }
